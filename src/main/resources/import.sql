5980dfe0ffc63ed56f6f788d4ec2b467f55b3b43
DESKTOP-3S7TGHG\davyk <david.j.kavanagh@mycit.ie>
2016-11-14 21:40:04 +0000

    minor fix in import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,9 +2,9 @@ INSERT INTO Users (id, full_name, user_name, password, credit_limit) VALUES (1, 
 INSERT INTO Users (id, full_name, user_name, password, credit_limit) VALUES (2, 'Thomas O Halloran', 'tamoh1', 147258369, 3000);
 INSERT INTO Users (id, full_name, user_name, password, credit_limit) VALUES (3, 'Darren Smith', 'foxylocks', 987654321, 5500);
 
-INSERT INTO Projects (id, name, description, image, goal_amt, time_limit) VALUES (1, 'Trickstick', 'unique, high res, handheld LED POV display.', 'someURL', 98000, 30);
-INSERT INTO Projects (id, name, description, image, goal_amt, time_limit) VALUES (2, 'Dialect: A Game about Language and How it Dies', 'Build a language, build a world.', 'someURL', 3000, 15);
-INSERT INTO Projects (id, name, description, image, goal_amt, time_limit) VALUES (3, 'Findster Duo - The 1st GPS Pet Tracker Free of Monthly Fees!', 'Findster Duo tracks your pets location using GPS with no data fees.', 'someURL', 50000, 45);
+INSERT INTO Projects (id, name, description, image, goal_Amt, time_Limit) VALUES (1, 'Trickstick', 'The Trickstick is a unique, high res, handheld LED POV display. Wave it and a colorful, detailed image appears in thin air – Magic!', 'https://ksr-ugc.imgix.net/assets/014/337/274/29ae0199e1917fb66c0909bfdb05e79a_original.jpg?w=680&fit=max&v=1477958772&auto=format&q=92&s=2b4d83c488c1f133514b1a4191287c2f', 98000, 30);
+INSERT INTO Projects (id, name, description, image, goal_Amt, time_Limit) VALUES (2, 'Dialect: A Game about Language and How it Dies', 'Build a language, build a world. Who were the Isolation, and how were they lost? In this game, your language is the story.', 'https://ksr-ugc.imgix.net/assets/014/233/249/f0daaca7912d700d58bd3e060588e987_original.png?w=680&fit=max&v=1477272636&auto=format&lossless=true&s=e252984bb5348f17783ef41b0bf1a7e5', 3000, 15);
+INSERT INTO Projects (id, name, description, image, goal_Amt, time_Limit) VALUES (3, 'Findster Duo - The 1st GPS Pet Tracker Free of Monthly Fees!', 'Findster Duo tracks your pets’ location using GPS with no data fees, and rewards you for keeping them healthy! Smart. Connected. Fun.', 'https://ksr-ugc.imgix.net/assets/012/925/070/6710c78aee17b9225cf4ccd1a1194f7f_original.png?w=680&fit=max&v=1467382915&auto=format&lossless=true&s=e412ed1cd0b66d859de6a3eec4fcaf80', 50000, 45);
 
 INSERT INTO Pledges VALUES (1, 150, false, 1);
 INSERT INTO Pledges VALUES (2, 100, false, 1);
