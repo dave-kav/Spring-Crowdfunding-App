4798e3db6992bae4384f187ecf62fdacca14693a
miniboc <Darren@DESKTOP-F2TMUFC>
2016-11-15 19:27:22 +0000

    added change page on login

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -3,4 +3,12 @@
 	 
 	 $('form').fadeOut(500);
 	 $('.wrapper').addClass('form-success');
+	 
+	 $(document).ready(function () {
+		    // Handler for .ready() called.
+		    window.setTimeout(function () {
+		        location.href = "https://www.google.com";
+		    }, 5000);
+		});
+
 });
